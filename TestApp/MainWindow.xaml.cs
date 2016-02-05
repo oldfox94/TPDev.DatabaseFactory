27ea9236d9d2999e35bff9bc5f5259bd4124ac46
oldfox94 <raphael@tpdev.theproducers.at>
2016-02-05 18:48:31 +0100

    DatabaseFactory.TestApp: - TestApp wurde hinzugefügt.

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using DatabaseFactory;
+using DbInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DbFactory m_dbFactory { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var connectionData = new DbConnectionData();
+
+            //SQL
+            connectionData.ServerName = "";
+            connectionData.Name = "";
+            connectionData.User = "";
+            connectionData.Password = "";
+            //m_dbFactory = new DbFactory.DbFactory(DbType.SQL, connectionData);
+
+            //SQLite
+            connectionData.Path = Environment.CurrentDirectory;
+            connectionData.Name = "D_TestApp";
+            m_dbFactory = new DbFactory(DbType.SQLite, connectionData);
+
+            //MySQL
+            connectionData.ServerName = "";
+            connectionData.Port = "";
+            connectionData.Name = "";
+            connectionData.User = "";
+            connectionData.Password = "";
+            //m_dbFactory = new DbFactory.DbFactory(DbType.MySQL, connectionData);
+
+            m_dbFactory.InitLogger("DbFactoryLog");
+        }
+
+        private void CreateTable()
+        {
+            var columns = GetSampleColumns();
+            m_dbFactory.Insert.CreateTable("TestTbl", columns);
+        }
+
+        private List<ColumnData> GetSampleColumns()
+        {
+            var colList = new List<ColumnData>();
+
+            colList.Add(new ColumnData { Name = "Pk", Type = DbDEF.TxtUniNotNullPk});
+            colList.Add(new ColumnData { Name = "Name", Type = DbDEF.TxtNull });
+            colList.Add(new ColumnData { Name = "Text", Type = DbDEF.TxtNull });
+
+            return colList;
+        }
+    }
+}
