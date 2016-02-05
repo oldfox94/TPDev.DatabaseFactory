7d1fcba1c76303de390596c58d6a5543c08d0485
oldfox94 <raphael@tpdev.theproducers.at>
2016-02-05 18:58:16 +0100

    DatabaseFactory.DbInterface: - Enums hinzugefügt

--- DbInterface/Models/Enums.cs
+++ DbInterface/Models/Enums.cs
@@ -6,4 +6,29 @@
         SQLite,
         MySQL,
     }
+
+    public static class DbORDER
+    {
+        public static string Ascending = "ASC";
+        public static string Descending = "DESC";
+    }
+
+    public static class DbOPERATOR
+    {
+        public const string Smaller = "<";
+        public static string Greater = ">";
+        public static string Is = "=";
+        public static string IsNot = "!=";
+    }
+
+    public static class DbDEF
+    {
+        public const string TxtNull = "TEXT  NULL";
+        public const string TxtNotNull = "TEXT  NOT NULL";
+        public const string TxtNotNullPk = "TEXT  NOT NULL PRIMARY KEY";
+        public const string TxtUniNotNullPk = "TEXT  UNIQUE NOT NULL PRIMARY KEY";
+
+        public const string BoolNull = "BOOLEAN  NULL";
+        public const string BoolNotNull = "BOOLEAN  NOT NULL";
+    }
 }
