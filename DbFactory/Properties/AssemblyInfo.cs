de0d66819ac0940389d4716444eeb3f0998d029c
oldfox94 <raphael@tpdev.theproducers.at>
2016-02-22 21:53:39 +0100

    DatabaseFactory: - AssemblyInfo angepasst.

--- DbFactory/Properties/AssemblyInfo.cs
+++ DbFactory/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("DbFactory")]
+//[assembly: AssemblyTitle("TPDev.DatabaseFactory")]
 //[assembly: AssemblyDescription("")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
