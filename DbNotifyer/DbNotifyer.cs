dd84bda8750b17eb25c06effc1af2909e94d1bde
Raphael <office@tpdev.at>
2016-02-24 14:42:18 +0100

    DatabaseFactory: - DbNotifyer Initialisierung grundstruktur.

--- DbNotifyer/DbNotifyer.cs
+++ DbNotifyer/DbNotifyer.cs
@@ -1,6 +1,15 @@
-﻿namespace DbNotifyer
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Drawing;
+
+namespace DbNotifyer
 {
     public class DbNotifyer
     {
+        private TaskbarIcon m_tbNotify { get; set; }
+        public DbNotifyer()
+        {
+            m_tbNotify = new TaskbarIcon();
+            m_tbNotify.Icon = new Icon("/DbNotifyer;component/Images/Database.ico");
+        }
     }
 }
