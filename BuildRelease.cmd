7fc44854f7c0a257cf7c76605f42fa18c840ec5c
oldfox94 <raphael@tpdev.theproducers.at>
2016-02-25 18:53:21 +0100

    DatabaseFactory: - BuildRelease aktualisiert

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -1,4 +1,5 @@
 echo Copy files ...
+copy DbNotifyer\bin\Release\Hardcodet.Wpf.TaskbarNotification DbFactory\Bin\Release
 copy SQLiteLibrary\bin\Release\System.Data.SQLite.dll DbFactory\bin\Release
 copy SQLiteLibrary\SQLite.Interop.dll DbFactory\bin\Release
 copy MySQLLibrary\bin\Release\MySql.Data.dll DbFactory\bin\Release
@@ -9,22 +10,25 @@ copy MySQLLibrary\bin\Release\MySql.Fabric.Plugin.dll DbFactory\bin\Release
 
 echo Building Project ...
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" DbInterface\DbInterface.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
-
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" DbFactory\DbFactory.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
 
 
 echo Cleanup Release folder ...
 del Release\*.*  /s /q
 
-
-echo Merging Binaries ...
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /ndebug /copyattrs /targetplatform:4.0,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:Release\TPDev.DatabaseFactory.dll Binaries\DatabaseFactory.dll Binaries\DbInterface.dll Binaries\DbLogger.dll Binaries\MySQLLibrary.dll Binaries\SQLiteLibrary.dll Binaries\SQLLibrary.dll
-
-
 echo Copy new dll files ...
+copy DbFactory\bin\Release\*.dll Binaries
+copy DbFactory\bin\Release\*.pdb Binaries
+
+copy 
+copy Binaries\Hardcodet.Wpf.TaskbarNotification.dll Release
 copy Binaries\System.Data.SQLite.dll Release
 copy SQLiteLibrary\SQLite.Interop.dll Release
 copy Binaries\MySql.Data.dll Release
+
+
+echo Merging Binaries ...
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /ndebug /copyattrs /targetplatform:4.0,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:Release\TPDev.DatabaseFactory.dll Binaries\DatabaseFactory.dll Binaries\DbInterface.dll Binaries\DbLogger.dll Binaries\DbNotifyer.dll Binaries\MySQLLibrary.dll Binaries\SQLiteLibrary.dll Binaries\SQLLibrary.dll
 
 echo finished!
 pause
