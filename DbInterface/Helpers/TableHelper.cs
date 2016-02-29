b34acaa5535e331e1e68179b55830aa5c1c32331
oldfox94 <raphael@tpdev.theproducers.at>
2016-02-29 07:17:13 +0100

    DatabaseFactory: - BugFix: TableHelper: Bei Update Table wurden nicht alle Rows durchgeändert -- Es wurde kein DsStatus geschrieben -- Es wurde kein InsertOn und Modify On aktualisiert.

--- DbInterface/Helpers/TableHelper.cs
+++ DbInterface/Helpers/TableHelper.cs
@@ -10,16 +10,18 @@ namespace DbInterface.Helpers
         {
             foreach(DataRow dr in tbl.Rows)
             {
-                if (dr.RowState == DataRowState.Unchanged) return;
+                if (dr.RowState == DataRowState.Unchanged) continue;
 
                 if (dr.RowState == DataRowState.Added)
                 {
                     dr[DbCIC.InsertOn] = DateTime.Now.ToString();
+                    dr[DbCIC.DsStatus] = "1";
                 }
 
                 if(dr.RowState == DataRowState.Modified || dr.RowState == DataRowState.Detached)
                 {
                     dr[DbCIC.ModifyOn] = DateTime.Now.ToString();
+                    if(string.IsNullOrEmpty(dr[DbCIC.DsStatus].ToString())) dr[DbCIC.DsStatus] = "1";
                 }
             }
         }
