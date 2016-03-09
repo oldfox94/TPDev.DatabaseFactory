52fb66202f6fe88ead9a9f6c3182cf45f18b4c4d
oldfox94 <raphael@tpdev.theproducers.at>
2016-03-09 18:31:25 +0100

    DatabaseFactory: - RELEASE V1.0.0.6

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
 [assembly: AssemblyDescription("Beta")] // Testversion, RC1, RC2,  Rev. 1,
