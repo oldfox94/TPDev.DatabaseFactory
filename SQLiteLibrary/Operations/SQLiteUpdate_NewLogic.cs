7f8177898266c6e642b2c5a1c03b01e13bcf140b
Raphael Hackel <office@tpdev.at>
2018-02-07 15:55:04 +0100

    - Neue Logic für SQLite Updates => Zuerst Testen dann übernehmen!!

---
+++ SQLiteLibrary/Operations/SQLiteUpdate_NewLogic.cs
@@ -0,0 +1,173 @@
+﻿using DbInterface;
+using DbInterface.Helpers;
+using DbInterface.Interfaces;
+using DbInterface.Models;
+using DbLogger.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SQLiteLibrary.Operations
+{
+    public class SQLiteUpdate : IUpdateOperations
+    {
+        SQLiteExecute m_Execute { get; set; }
+        public SQLiteUpdate()
+        {
+            m_Execute = new SQLiteExecute();
+        }
+
+        public bool UpdateDataSet(DataSet dataSet)
+        {
+            try
+            {
+                using (var con = new SQLiteConnection(Settings.ConnectionString))
+                {
+                    foreach (DataTable tbl in dataSet.Tables)
+                    {
+                        using (var adapter = new SQLiteDataAdapter(string.Format(@"SELECT * FROM {0} WHERE 1=0", tbl.TableName), con))
+                        {
+                            using (var cmd = new SQLiteCommandBuilder(adapter))
+                            {
+                                adapter.Update(tbl);
+                                cmd.Dispose();
+                            }
+                            adapter.Dispose();
+                        }
+                    }
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateDataSet Error!",
+                    Ex = ex,
+                });
+                if (Settings.ThrowExceptions) throw new Exception("UpdateDataSet Error!", ex);
+                return false;
+            }
+        }
+
+        public bool UpdateOneValue(string tableName, string column, string value, string where)
+        {
+            try
+            {
+                var whereCnd = ConvertionHelper.GetWhere(where);
+
+                var sql = string.Format(@"UPDATE {0} SET {1} = '{2}', {3} = '{4}' {5}",
+                            tableName, column, ConvertionHelper.CleanStringForSQL(value), DbCIC.ModifyOn, DateTime.Now.ToString(), whereCnd);
+                var result = m_Execute.ExecuteNonQuery(sql);
+
+                if (result == -2) return false;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateOneValue Error!",
+                    Ex = ex,
+                });
+                if (Settings.ThrowExceptions) throw new Exception("UpdateOneValue Error!", ex);
+                return false;
+            }
+        }
+
+        public bool UpdateTable(DataTable table)
+        {
+            try
+            {
+                var tableName = table.TableName;
+                return UpdateTable(table, tableName);
+            }
+            catch(Exception ex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateTable Error!",
+                    Ex = ex,
+                });
+                if (Settings.ThrowExceptions) throw new Exception("UpdateTable Error!", ex);
+                return false;
+            }
+        }
+
+        public bool UpdateTable(DataTable table, string tableName)
+        {
+            try
+            {
+                TableHelper.SetDefaultColumnValues(table);
+
+                //var con = CONNECTION.OpenCon();
+                using (var con = new SQLiteConnection(Settings.ConnectionString))
+                {
+                    using (var adapter = new SQLiteDataAdapter(string.Format(@"SELECT * FROM {0} WHERE 1=0", tableName), con))
+                    {
+                        using (var cmd = new SQLiteCommandBuilder(adapter))
+                        {
+                            adapter.Update(table);
+                            cmd.Dispose();
+                        }
+                        adapter.Dispose();
+                    }
+                }
+                //CONNECTION.CloseCon(con);
+                return true;
+            }
+            catch (DBConcurrencyException cex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateTable DBConcurrencyError!",
+                    Ex = cex,
+                });
+                if (Settings.ThrowExceptions) throw new DBConcurrencyException("UpdateTable Error!", cex);
+                return false;
+            }
+            catch(Exception ex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateTable Error!",
+                    Ex = ex,
+                });
+                if (Settings.ThrowExceptions) throw new Exception("UpdateTable Error!", ex);
+                return false;
+            }
+        }
+
+        public bool UpdateTables(List<DataTable> tableList)
+        {
+            try
+            {
+                var result = false;
+                foreach(DataTable tbl in tableList)
+                {
+                    result = UpdateTable(tbl);
+                    if (!result) return result;
+                }
+
+                return result;
+            }
+            catch(Exception ex)
+            {
+                SLLog.WriteError(new LogData
+                {
+                    Source = ToString(),
+                    FunctionName = "UpdateTables Error!",
+                    Ex = ex,
+                });
+                if (Settings.ThrowExceptions) throw new Exception("UpdateTables Error!", ex);
+                return false;
+            }
+        }
+    }
+}
