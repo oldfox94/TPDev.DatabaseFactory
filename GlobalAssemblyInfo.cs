49b2e86373fb99320ef5ae0c682a5f75a958f159
Raphael Hackel <office@tpdev.at>
2023-01-26 13:14:09 +0100

    - Debug & Release Version 1.0.1.1

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
 [assembly: AssemblyDescription("")] // Testversion, RC1, RC2,  Rev. 1,
